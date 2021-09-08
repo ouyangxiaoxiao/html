adbdc9fbf3ca62ef234113228384776326f3f8dc
ouyangxiaoxiao <284473823@qq.com>
2021-09-08 16:12:49 +0800

    22

--- 消防设施操作员/js/play.js
+++ 消防设施操作员/js/play.js
@@ -184,7 +184,7 @@ window.addEventListener('load', function () {
         for (i = 5; i <= lis.length; i++) {
             // console.log(lis[i]);
             // console.log(lis[i].index);
-            lis[i].setAttribute("value", "https://ouyangxiaoxiao.github.io/xiaofangshipin/五级(初级)实操/qcaqjfwdljyo574.mp4");
+            lis[i].setAttribute("value", "登录");
             // lis[i].value =  "登录";
             // console.log(55555)
 
@@ -212,7 +212,7 @@ function myFunction() {
 
     } else {
         zhezhaoceng.style.display = "none";
-        // video.style.display = "block";
+        video.style.display = "block";
     }
 
 
