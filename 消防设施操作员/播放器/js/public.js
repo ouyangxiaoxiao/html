00899456e93fb8db52470c61054be4acf5be9ba0
ouyangxiaoxiao <284473823@qq.com>
2021-09-19 15:37:41 +0800

    22

--- 消防设施操作员/播放器/js/public.js
+++ 消防设施操作员/播放器/js/public.js
@@ -1,14 +1,16 @@
 window.addEventListener("load", function () {
     var kechengtanchuang = document.querySelector("#kechengtanchuang");
-    console.log(kechengtanchuang);
     var tanchuangxianshi = document.querySelector(".tanchuangxianshi");
-    console.log(tanchuangxianshi);
+    var bofangqi = document.querySelector(".bofangqi");
     kechengtanchuang.onclick = function () {
         // alert("点击");
         if (tanchuangxianshi.style.display == "block") {
             tanchuangxianshi.style.display = "none";
+            bofangqi.style.display = "block"
+
         } else {
             tanchuangxianshi.style.display = "block";
+            bofangqi.style.display = "none"
         }
     };
 });
