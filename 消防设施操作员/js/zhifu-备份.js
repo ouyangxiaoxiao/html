70fcaef6cf5e31f5b33cf52dc01bc9d50ac26ebd
ouyangxiaoxiao <284473823@qq.com>
2021-09-20 08:14:41 +0800

    55

--- 消防设施操作员/js/zhifu-备份.js
+++ 消防设施操作员/js/zhifu-备份.js
@@ -3,31 +3,38 @@ window.addEventListener("load", function () {
     var right = document.querySelector(".right");
     var list = document.querySelector(".list");
     var jglf = document.querySelector("#jq_vipPriceType1");
-
-
+    combined = 0;
+    count = 0;
     console.log(list);
     /*下册摁钮*/
     lis = list.querySelectorAll("li");
     console.log(lis.length);
     for (i = 0; i < lis.length; i++) {
         lis[i].onclick = function () {
-            right.classList.remove("xuanzhong");
+            /*获取价格*/
+            nub = this.querySelector("i").querySelector("span").innerText;
             left.classList.add("xuanzhong");
+            right.classList.remove("xuanzhong");
+            console.log(left);
+            console.log(555555555);
+
+
             if (this.classList.contains("xuanzhong")) {
                 this.classList.remove("xuanzhong");
+                /*选中加1次*/
+                count -= 1;
+                combined -= parseInt(nub);
+
             } else {
                 this.classList.add("xuanzhong");
+                /*选中加1次*/
+                count += 1;
+                console.log(parseInt(nub));
+                combined += parseInt(nub);
+                console.log("合计：" + combined)
+
             }
-            combined = 0;
-            count = 0;
-            for (i = 0; i < lis.length; i++) {
-                if (lis[i].classList.contains("xuanzhong")) {
-                    nub = lis[i].querySelector("i").querySelector("span").innerText;
-                    combined += parseInt(nub);
-                    count += 1;
-                    console.log(combined);
-                }
-            }
+
             /*计价*/
             jglf.innerHTML = "已选" + count + "课共计：" + combined + "元 ";
 
@@ -37,36 +44,30 @@ window.addEventListener("load", function () {
 
     /*左侧按课选择*/
     left.onclick = function () {
-        combined = 0;
-        count = 0;
-        for (i = 0; i < lis.length; i++) {
-            if (lis[i].classList.contains("xuanzhong")) {
-                nub = lis[i].querySelector("i").querySelector("span").innerText;
-                combined += parseInt(nub);
-                count += 1;
-                console.log(combined);
-            }
-        }
-
-        /*计价*/
-        jglf.innerHTML = "已选" + count + "课共计：" + combined + "元 ";
 
         if (this.classList.contains("xuanzhong")) {
             this.classList.remove("xuanzhong");
             right.classList.remove("xuanzhong");
+            /*下面批量勾选*/
+            for (i = 0; i < lis.length; i++) {
+
+                lis[i].classList.remove("xuanzhong");
+
+            }
 
         } else {
             this.classList.add("xuanzhong");
             this.classList.add("xuanzhong");
             right.classList.remove("xuanzhong");
-
+            /*下面取消批量勾选*/
+            for (i = 0; i < lis.length; i++) {
+                lis[i].classList.remove("xuanzhong");
+            }
         }
 
     };
     /*右侧全选*/
     right.onclick = function () {
-        /*计价*/
-        jglf.innerHTML = "已选" + 0 + "课共计：" + 0 + "元 ";
         if (this.classList.contains("xuanzhong")) {
             this.classList.remove("xuanzhong");
             left.classList.remove("xuanzhong");
